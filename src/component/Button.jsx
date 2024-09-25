bd8a7c195f28299dcff4efc629139fe711280d21
jinhyeon-dev <jinhyeon.dev@gmail.com>
2024-09-25 11:22:55 +0900

    9/25

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,8 +1,10 @@
 import './Button.css'
 
 const Button = ({text, type, onClick}) => {
+    const btnType = ["positive", "negative"].includes(type) ? type : "default"
+
     return (
-        <button className='Button' onClick={{onClick}}>{text}</button>
+        <button className={["Button", `Button_${btnType}`].join(" ")} onClick={{onClick}}>{text}</button>
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,20 @@ import Button from "../component/Button"
 const Home = () => {
      return (
         <div>
-            <Button />
+            <Button 
+                text={"기본"}
+                onClick={() => alert('기본')}
+            />
+            <Button 
+                type="positive"
+                text={"긍정"}
+                onClick={() => alert('긍정')}
+            />
+            <Button 
+                type="negative"
+                text={"부정"}
+                onClick={() => alert('부정')}
+            />
         </div>
      )
 }
