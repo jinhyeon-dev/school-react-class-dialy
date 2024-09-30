a3a15996e2358fb1d0c086565d6b81ff481a5f41
jinhyeon-dev <jinhyeon.dev@gmail.com>
2024-09-30 11:22:12 +0900

    9/30

--- src/component/EmotionItem.jsx
+++ src/component/EmotionItem.jsx
@@ -6,7 +6,7 @@ const EmotionItem = ({ id, img, name, onClick, isSelected }) => {
         onClick(id)
     }
     return (
-        <div className='EmotionItem' onClick={handleOnClick}>
+        <div className={['EmotionItem', isSelected ? `EmotionItem_on_${id}` : `EmotionItem_off`,].join(" ")} onClick={handleOnClick}>
             <img alt={`emotion${id}`} src={img} />
         </div>
     )
